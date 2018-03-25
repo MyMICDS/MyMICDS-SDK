22615961e570b6f76e6f8c33dfd0f7f27b651b87
AsianPsychoBoy <ziyang_99@outlook.com>
2018-03-25 17:37:41 -0600

    whoops

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-// For .. path configuration
-import 'module-alias/register';
-
 import { MyMICDSError } from './error';
 import { MyMICDSOptions } from './options';
 import { MyMICDS } from './sdk';
