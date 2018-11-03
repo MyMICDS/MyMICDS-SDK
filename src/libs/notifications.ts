9ebdc1a953227f194a0142e4e3c5567560604a08
Michael Gira <contact@michaelgira.me>
2018-11-03 17:20:00 -0500

    Add parameters for notification unsubscribe

--- src/libs/notifications.ts
+++ src/libs/notifications.ts
@@ -8,8 +8,18 @@ import { HTTP } from '../http';
 export class NotificationsAPI {
 	constructor(private http: HTTP) { }
 
-	unsubscribe() {
-		return this.http.post('/notifications/unsubscribe');
+	unsubscribe(param: UnsubscribeParameters) {
+		return this.http.post('/notifications/unsubscribe', param);
 	}
 
+}
+
+/**
+ * API Parameters and Responses
+ */
+
+export interface UnsubscribeParameters {
+	user: string;
+	hash: string;
+	type: string;
 }
