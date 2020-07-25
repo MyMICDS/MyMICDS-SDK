37cc13fdd0791c4a61eaa3d0c3c84b4a718c3f71
Sam B <28550422+MeMeBigProgrammer@users.noreply.github.com>
2020-07-25 17:23:42 -0500

    Migrate weather (#14)
    * add new weather type
    
    * upgrade ts (needs work) add new types
    
    * fixing types, 
    
    need some cleanup and naming, but it should work for now
    
    * naming and cleanup
    
    * requested changes
    
    wait... it's all spaces? 
    
    Always has been

--- src/libs/weather.ts
+++ src/libs/weather.ts
@@ -12,34 +12,7 @@ export class WeatherAPI {
 	constructor(private http: HTTP) { }
 
 	get(shouldError = false) {
-		return this.http.get<GetWeatherResponse>('/weather', shouldError).pipe(
-			tap(r => {
-				const w = r.weather;
-
-				w.currently.time = moment.unix(w.currently.time as any);
-
-				for (const minute of w.minutely.data) {
-					minute.time = moment.unix(minute.time as any);
-				}
-
-				// Oof
-				for (const day of w.daily.data) {
-					day.sunriseTime = moment.unix(day.sunriseTime as any);
-					day.sunsetTime = moment.unix(day.sunsetTime as any);
-					day.precipIntensityMaxTime = moment.unix(day.precipIntensityMaxTime as any);
-					day.temperatureHighTime = moment.unix(day.temperatureHighTime as any);
-					day.temperatureLowTime = moment.unix(day.temperatureLowTime as any);
-					day.apparentTemperatureHighTime = moment.unix(day.apparentTemperatureHighTime as any);
-					day.apparentTemperatureLowTime = moment.unix(day.apparentTemperatureLowTime as any);
-					day.windGustTime = moment.unix(day.windGustTime as any);
-					day.uvIndexTime = moment.unix(day.uvIndexTime as any);
-					day.temperatureMinTime = moment.unix(day.temperatureMinTime as any);
-					day.temperatureMaxTime = moment.unix(day.temperatureMaxTime as any);
-					day.apparentTemperatureMinTime = moment.unix(day.apparentTemperatureMinTime as any);
-					day.apparentTemperatureMaxTime = moment.unix(day.apparentTemperatureMaxTime as any);
-				}
-			})
-		);
+		return this.http.get<GetWeatherResponse>('/weather', shouldError);
 	}
 
 	update(shouldError = false) {
@@ -48,88 +21,17 @@ export class WeatherAPI {
 
 }
 
-/**
- * API Parameters and Responses
- */
-
 export interface GetWeatherResponse {
 	weather: Weather;
 }
 
-/**
- * Helpers
- */
-
-export interface Weather {
-	latitude: number;
-	longitude: number;
-	timezone: string;
-	currently: HourlyWeatherSnapshot;
-	minutely: WeatherDataGroup<MinutelyWeatherSnapshot>;
-	hourly: WeatherDataGroup<HourlyWeatherSnapshot>;
-	daily: WeatherDataGroup<DailyWeatherSnapshot>;
-	flags: {
-		sources: string[];
-		'nearest-station': number;
-		units: string;
-	};
-	offset: number;
-}
-
-export interface SummaryIcon {
-	summary: string;
-	icon: string;
-}
-
-export interface WeatherDataGroup<T> extends SummaryIcon {
-	data: T[];
-}
-
-export interface MinutelyWeatherSnapshot {
-	time: moment.Moment;
-	precipIntensity: number;
-	precipProbability: number;
-}
-
-export interface HourlyWeatherSnapshot extends SummaryIcon, MinutelyWeatherSnapshot {
-	nearestStormDistance: number;
-	nearestStormBearing: number;
+export interface Weather { // all in fahrenheit, miles/hour, imperial
 	temperature: number;
-	apparentTemperature: number;
-	dewPoint: number;
-	humidity: number;
-	pressure: number;
-	windSpeed: number;
-	windGust: number;
-	windBearing: number;
-	cloudCover: number;
-	uvIndex: number;
-	visibility: number;
-	ozone: number;
-}
-
-export interface DailyWeatherSnapshot extends HourlyWeatherSnapshot {
-	sunriseTime: moment.Moment;
-	sunsetTime: moment.Moment;
-	moonPhase: number;
-	precipIntensityMax: number;
-	precipIntensityMaxTime: moment.Moment;
 	temperatureHigh: number;
-	temperatureHighTime: moment.Moment;
 	temperatureLow: number;
-	temperatureLowTime: moment.Moment;
-	apparentTemperatureHigh: number;
-	apparentTemperatureHighTime: moment.Moment;
-	apparentTemperatureLow: number;
-	apparentTemperatureLowTime: moment.Moment;
-	windGustTime: moment.Moment;
-	uvIndexTime: moment.Moment;
-	temperatureMin: number;
-	temperatureMinTime: moment.Moment;
-	temperatureMax: number;
-	temperatureMaxTime: moment.Moment;
-	apparentTemperatureMin: number;
-	apparentTemperatureMinTime: moment.Moment;
-	apparentTemperatureMax: number;
-	apparentTemperatureMaxTime: moment.Moment;
+	humidity: number;
+	precipitationChance: number;
+	windSpeed: number;
+	windDir: number;
+	weatherIcon: string;
 }
